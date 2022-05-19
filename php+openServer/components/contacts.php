dae3f948b204fbda2caea79f6a50cb873889dc95
Sergey Andreev <ozwatandree@gmail.com>
2022-05-19 19:21:40 +0300

    php

---
+++ php+openServer/components/contacts.php
@@ -0,0 +1,10 @@
+<section class="contacts container">
+  <h2 class="section-title">Контакты для связи</h2>
+    <img src="<?= $image ?>" width="146" height="146" alt="<?= $name ?>">
+    <div class="contacts-content">
+      <p class="contacts-name"><?= $name ?></p>
+      <p><i>email:</i> <a href="mailto:<?= $email ?>"><?= $email ?></a></p>
+      <p><i>телефон:</i> <a href="tel:<?= $phone ?>"><?= $phone ?></a></p>
+      <p> <i>Telegram:</i> <a href="https://t.me/<?= $tg ?>"> <? = $tg ?></a></p>
+    </div>
+</section>
