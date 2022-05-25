c2a976a2838bab667f913d1c4d4a33e0a6096f4d
Sergey Andreev <ozwatandree@gmail.com>
2022-05-26 00:04:27 +0300

    skils90%
    profu hard ostalos

--- phpOpenServer/components/header.php
+++ phpOpenServer/components/header.php
@@ -15,15 +15,15 @@
       <div Class="info">
       <p > QA Engineer/Тестировщик</p>
     <p >Возраст:<b>21год</b></p>
-    <p >Место жительства: <a href="https://www.google.ru/maps/place/%D0%A7%D0%B5%D0%B1%D0%BE%D0%BA%D1%81%D0%B0%D1%80%D1%8B,+%D0%A7%D1%83%D0%B2%D0%B0%D1%88%D1%81%D0%BA%D0%B0%D1%8F+%D0%A0%D0%B5%D1%81%D0%BF./@56.1042955,47.1893787,12z/data=!3m1!4b1!4m5!3m4!1s0x415a37f37d16e915:0x7174067761ec11d9!8m2!3d56.1168498!4d47.2628403"><b>г.Чебоксары</b></a></p>
+    <p >Место жительства: <a target="_blank" href="https://www.google.ru/maps/place/%D0%A7%D0%B5%D0%B1%D0%BE%D0%BA%D1%81%D0%B0%D1%80%D1%8B,+%D0%A7%D1%83%D0%B2%D0%B0%D1%88%D1%81%D0%BA%D0%B0%D1%8F+%D0%A0%D0%B5%D1%81%D0%BF./@56.1042955,47.1893787,12z/data=!3m1!4b1!4m5!3m4!1s0x415a37f37d16e915:0x7174067761ec11d9!8m2!3d56.1168498!4d47.2628403"><b>г.Чебоксары</b></a></p>
      </div>
 
 <div Class="info2">
       <p> <i >Email:</i> <a href="mailto:<?= $email ?>"><?= $email ?></a> </p>
       <p ><i>Телефон:</i> <a href="tel:<?= $phone ?>"><?= $phone ?></a></p>
-      <p ><i>Telegram:</i> <a href="https://t.me/<?= $tg ?>"><?= $tg ?></a></p>
-       <p ><i>GitHub:</i> <a href="https://github.com/mrozwat/Andreev_qa">mrozwat</a></p>
-       <p ><i>Сайт:</i> <a href="https://ozwat.ru">ozwat.ru</a></p>
+      <p ><i>Telegram:</i> <a target="_blank" href="https://t.me/<?= $tg ?>"><?= $tg ?></a></p>
+       <p ><i>GitHub:</i> <a target="_blank" href="https://github.com/mrozwat/Andreev_qa">mrozwat</a></p>
+       <p ><i>Сайт:</i> <a target="_blank" href="https://ozwat.ru">ozwat.ru</a></p>
 </div>
 
 <button  class="theme-button" type="button"  >Изменить тему</button>
--- phpOpenServer/components/skils.php
+++ phpOpenServer/components/skils.php
@@ -1,43 +1,48 @@
 <section class="container">
   <div class="skills">
-  <h2>Мои навыки</h2>
+  <div class="center"> <h2>Мои навыки</h2></div>
   <h3><a href="https://github.com/mrozwat/Andreev_qa">Hard Skils</a></h3>
   <dl class="skills-list">
     <dt class="skill-html">HTML</dt>
-    <dd class="level"><div style="width: 60%;">60%</div></dd>
+    <dd class="level"><div style="width: 70%;">70%</div></dd>
     <dt class="skill-css">CSS</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-     <dt class="skill-js">JS</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-php">PHP</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-sql">SQL</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-json">Json</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-api">Api(Rest/SOAP)</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-post">Postman</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-git">GitHub</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-android">AndroidStudio</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-proxy">CharlesProxy</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-     <dt class="skill-jira">Jira</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-     <dt class="skill-linux">Linux</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-qa">QA(теория)</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-    <dt class="skill-marketing">Marketing(b2b,аналитика, арбитраж трафика,SEO,таргет+контекст)</dt>
-    <dd class="level"><div style="width: 40%;">40%</div></dd>
-  </dl>
-</div>
-<h3><a href="https://docs.google.com/forms/d/e/1FAIpQLSeviwjzFrvLCsf3QMuBSdF-rkf3QUsYKCUnMcrU4vkcWg032g/viewscore?viewscore=AE0zAgD3TFiEla__0SGhvWUhQpIsKxyPt6fXc6c7S5aU49SGSPerzYTkyZ81Vq9lsUqQf0k">Soft Skils</a></h3>
-<dl class="skills-list">
-<dt class="skill-html">HTML</dt>
     <dd class="level"><div style="width: 60%;">60%</div></dd>
+     <dt class="skill-js">JS</dt>
+    <dd class="level"><div style="width: 30%;">30%</div></dd>
+    <dt class="skill-php">PHP</dt>
+    <dd class="level"><div style="width: 10%;">10%</div></dd>
+    <dt class="skill-sql">SQL</dt>
+    <dd class="level"><div style="width: 70%;">70%</div></dd>
+    <dt class="skill-json">Json</dt>
+    <dd class="level"><div style="width: 80%;">80%</div></dd>
+    <dt class="skill-api">Api(Rest/SOAP)</dt>
+    <dd class="level"><div style="width: 60%;">60%</div></dd>
+    <dt class="skill-post">Postman</dt>
+    <dd class="level"><div style="width: 70%;">70%</div></dd>
+    <dt class="skill-git">GitHub</dt>
+    <dd class="level"><div style="width: 90%;">90%</div></dd>
+    <dt class="skill-android">AndroidStudio</dt>
+    <dd class="level"><div style="width: 30%;">30%</div></dd>
+    <dt class="skill-proxy">CharlesProxy</dt>
+    <dd class="level"><div style="width: 80%;">80%</div></dd>
+     <dt class="skill-jira">Jira</dt>
+    <dd class="level"><div style="width: 50%;">50%</div></dd>
+     <dt class="skill-linux">Linux</dt>
+    <dd class="level"><div style="width: 70%;">70%</div></dd>
+    <dt class="skill-qa">QA(теория)</dt>
+    <dd class="level"><div style="width: 90%;">90%</div></dd>
+    <dt class="skill-marketing">Marketing(b2b,аналитика, арбитраж трафика,SEO,таргет+контекст)</dt>
+    <dd class="level"><div style="width: 95%;">95%</div></dd>
+  </dl>
+  <div class="right">
+ <a  target="_blank" href="!!!!!!Добавь ссылку  затупок" target="_blank">Подробнее</a>
+</div>
+</div>
+<dl class="skills-list">
+<h3><a href="https://docs.google.com/forms/d/e/1FAIpQLSeviwjzFrvLCsf3QMuBSdF-rkf3QUsYKCUnMcrU4vkcWg032g/viewscore?viewscore=AE0zAgD3TFiEla__0SGhvWUhQpIsKxyPt6fXc6c7S5aU49SGSPerzYTkyZ81Vq9lsUqQf0k">Soft Skils</a></h3>
+<dt class="skill-soft">Soft Skils(Люди)</dt>
+    <dd class="level"><div style="width: 75%;">75%</div></dd>
+    <dt class="skill-dog">Soft Skils(<a  href="https://photos.app.goo.gl/QhKbu2ptEqEC3zkh9" target="_blank">Собаки</a>)</dt>
+    <dd class="level"><div style="width: 100%;">100%</div></dd>
 </dl>
 </section>
