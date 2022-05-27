bc88965e35b7ad0e960476d90e8198a7fc288152
Sergey Andreev <ozwatandree@gmail.com>
2022-05-27 19:51:02 +0300

    ostalo opisanie napisati

--- phpOpenServer/components/skils.php
+++ phpOpenServer/components/skils.php
@@ -5,25 +5,21 @@
   <dl class="skills-list">
     <dt class="skill-html">HTML</dt>
     <dd class="level"><div style="width: 70%;">70%</div></dd>
-    <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
+
     <dt class="skill-css">CSS</dt>
     <dd class="level"><div style="width: 60%;">60%</div></dd>
-    <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
      <dt class="skill-js">JS</dt>
     <dd class="level"><div style="width: 30%;">30%</div></dd>
-    <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
+    <dt class="skill-json">Json</dt>
+    <dd class="level"><div style="width: 80%;">80%</div></dd>
     <dt class="skill-php">PHP</dt>
     <dd class="level"><div style="width: 10%;">10%</div></dd>
     <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
     <dt class="skill-sql">SQL</dt>
     <dd class="level"><div style="width: 50%;">50%</div></dd>
     <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
-    <dt class="skill-json">Json</dt>
-    <dd class="level"><div style="width: 80%;">80%</div></dd>
-    <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
     <dt class="skill-api">Api(Rest/SOAP)</dt>
     <dd class="level"><div style="width: 60%;">60%</div></dd>
-    <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
     <dt class="skill-post">Postman</dt>
     <dd class="level"><div style="width: 70%;">70%</div></dd>
     <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
@@ -57,7 +53,6 @@
 <h3><a href="https://docs.google.com/forms/d/e/1FAIpQLSeviwjzFrvLCsf3QMuBSdF-rkf3QUsYKCUnMcrU4vkcWg032g/viewscore?viewscore=AE0zAgD3TFiEla__0SGhvWUhQpIsKxyPt6fXc6c7S5aU49SGSPerzYTkyZ81Vq9lsUqQf0k">Soft Skils</a></h3>
 <dt class="skill-soft">Soft Skils(Люди)</dt>
     <dd class="level"><div style="width: 75%;">75%</div></dd>
-    <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
     <dt class="skill-dog">Soft Skils(<a  href="https://photos.app.goo.gl/QhKbu2ptEqEC3zkh9" target="_blank">Собаки</a>)</dt>
     <dd class="level"><div style="width: 100%;">100%</div></dd>
     <div class="dis none"><p>Очень интересное описание моих html навыков</p></div>
