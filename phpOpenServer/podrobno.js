a239b9e0e87b3cbf73a9f87010b6b44caedfa7c0
Sergey Andreev <ozwatandree@gmail.com>
2022-05-27 22:22:23 +0300

    js peredelal na for of

--- phpOpenServer/podrobno.js
+++ phpOpenServer/podrobno.js
@@ -1,9 +1,15 @@
 
-let elements = document.getElementsByClassName("dis");
+let elements = document.getElementsByClassName("dis"); //querySelectorAll можно было, но хз 
 let button = document.querySelector('.button');
-button.onclick = function () {
+/*button.onclick = function () {
 
 for (var i = 0; i < elements.length; i++) {
     elements[i].classList.toggle('none');
+}
+};// FIX (можно сделать через for of, но я затупок, а переделывать лень )
+*/
+button.onclick = function () {
+for(let element of elements) {
+   element.classList.toggle('none');
 }
 };
