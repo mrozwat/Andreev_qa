fb50c5a437e574f1ededa1f19be9bef93deea445
Sergey Andreev <ozwatandree@gmail.com>
2022-05-27 23:12:21 +0300

    2 knopka rabotaet
    Это самый говнокод из говнокода (лучше переделать потом)

--- phpOpenServer/podrobno.js
+++ phpOpenServer/podrobno.js
@@ -1,15 +1,27 @@
 
-let elements = document.getElementsByClassName("dis"); //querySelectorAll можно было, но хз 
+let elements = document.getElementsByClassName("dis"); //querySelectorAll можно было, но хз
 let button = document.querySelector('.button');
-/*button.onclick = function () {
 
+/*button.onclick = function () {
 for (var i = 0; i < elements.length; i++) {
     elements[i].classList.toggle('none');
 }
 };// FIX (можно сделать через for of, но я затупок, а переделывать лень )
 */
+
 button.onclick = function () {
 for(let element of elements) {
    element.classList.toggle('none');
 }
 };
+//2 knopka
+let elements2 = document.getElementsByClassName("dis2"); //querySelectorAll можно было, но хз
+let button2 = document.querySelector('.button2');
+
+button2.onclick = function () {
+for(let element2 of elements2) {
+   element2.classList.toggle('none');
+}
+};
+
+// Мда, не код а пздц
