71f2739b6852c125e29d2c81da550435bf476766
Sergey Andreev <ozwatandree@gmail.com>
2022-05-28 15:44:20 +0300

    knopki svernyti podrodnee

--- phpOpenServer/podrobno.js
+++ phpOpenServer/podrobno.js
@@ -2,26 +2,35 @@
 let elements = document.getElementsByClassName("dis"); //querySelectorAll можно было, но хз
 let button = document.querySelector('.button');
 
-/*button.onclick = function () {
-for (var i = 0; i < elements.length; i++) {
-    elements[i].classList.toggle('none');
-}
-};// FIX (можно сделать через for of, но я затупок, а переделывать лень )
-*/
-
 button.onclick = function () {
+
 for(let element of elements) {
    element.classList.toggle('none');
 }
+if( button.textContent.length > 8 ){button.textContent = 'свернуть'}
+else {button.textContent = 'подробнее'}
+
 };
+
 //2 knopka
 let elements2 = document.getElementsByClassName("dis2"); //querySelectorAll можно было, но хз
 let button2 = document.querySelector('.button2');
 
 button2.onclick = function () {
+
 for(let element2 of elements2) {
    element2.classList.toggle('none');
+if( button.textContent.length > 8 ){button.textContent = 'свернуть'}
+else {button.textContent = 'подробнее'}
 }
 };
 
 // Мда, не код а пздц
+
+/*
+button.onclick = function () {
+for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.toggle('none');
+}
+};// FIX (можно сделать через for of, но я затупок, а переделывать лень )
+*/
