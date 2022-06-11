ef1c0a063fc040889a24c8b0fa4dd0073fd23228
Sergey Andreev <ozwatandree@gmail.com>
2022-06-12 01:32:30 +0300

    education Final

--- phpOpenServer/components/education.php
+++ phpOpenServer/components/education.php
@@ -1,7 +1,7 @@
 <section class="news container">
  <div class="center"> <h2 class="section-title">Оброзование и Курсы</h2></div>
   <article class="card">
-    <img src="img/biceps.svg" width="56" height="56" alt="HTML">
+    <img src="img/cmk.jpg" width="56" height="56" alt="HTML">
     <div class="card-content">
       <h3>Чебоксарский электромеханический колледж</h3>
       <p>Специальность: Компьютерные системы и комплексы</p>
@@ -9,7 +9,7 @@
     </div>
   </article>
   <article class="card">
-    <img src="img/keks-bane.svg" width="56" height="56" alt="HTML">
+    <img src="img/nix.jpg" width="56" height="56" alt="HTML">
     <div class="card-content">
       <h3>NIX Education </h3>
       <p>Курс Qa специалиста</p>
@@ -17,16 +17,16 @@
     </div>
   </article>
   <article class="card">
-    <img src="img/treasure-map.svg" width="56" height="56" alt="HTML">
+    <img src="img/qa.jpg" width="56" height="56" alt="HTML">
     <div class="card-content">
-      <h3>Курс Qa специалиста от Николая Кротова </h3>
+      <h3>Курс Николая Кротова </h3>
       <p>Полный курс Qa специалиста </p>
  
     </div>
   </article>
 
   <article class="card">
-    <img src="img/treasure-map.svg" width="56" height="56" alt="HTML">
+    <img src="img/sql.svg" width="56" height="56" alt="HTML">
     <div class="card-content">
       <h3>SQL Academy  </h3>
       <p>Учебник + большенство тество на знания SQL (достаточное для получения сетификата) </p>
@@ -34,7 +34,7 @@
   </article>
 
       <article class="card">
-    <img src="img/treasure-map.svg" width="56" height="56" alt="HTML">
+    <img src="img/html.png" width="56" height="56" alt="HTML" >
     <div class="card-content">
       <h3>Html Academy   </h3>
       <p>Тренажеры по HTML, CSS, Js </p>
@@ -42,7 +42,7 @@
   </article>
 
   <article class="card">
-    <img src="img/treasure-map.svg" width="56" height="56" alt="HTML">
+    <img src="img/logo-javascript.svg" width="56" height="56" alt="HTML">
     <div class="card-content">
       <h3>JavaScript  YouTube курсы    </h3>
       <p>В частности:Алексей Лещенко курс ООП JavaScript+JavaScript 2.0, <br>Различные лекции Тимура Шемсидинова  </p>
